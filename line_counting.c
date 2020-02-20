5d00d25b1597edd50ca56133f2709594369be2b9
Ongun Arisev <ongunarisev@gmail.com>
2020-02-21 00:49:18 +0100

    Correct the file line_counting.c

--- line_counting.c
+++ line_counting.c
@@ -2,11 +2,15 @@
 /* let's count the lines in input */
 int main()
 {
-    int c;
+    int c, n1, n2, n3;
+
+    n1 = 0;
+    n2 = 0;
+    n3 = 0;
 
     while ((c = getchar()) != EOF)
-        if (c == '\\')
-            n1;
+        if (c == '\n')
+            ++n1;
         else if (c == '\t')
             ++n2;
         else if (c == ' ')

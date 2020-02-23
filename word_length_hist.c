99bb5f97348bd847b10b24d43e426531efb2c04f
Ongun Arisev <ongunarisev@gmail.com>
2020-02-23 02:08:18 +0100

    Initialize the values of array
    + Better readability for for loops
    + Corrected code for the histogram
    - Commented out the print statements

--- word_length_hist.c
+++ word_length_hist.c
@@ -10,6 +10,11 @@ int main()
 
     state = OUT;
     nword = 0;
+    
+    /* This step is really important to set the initial values of the array */
+    for (int i = 0; i < 20; ++i)
+        hist[i] = 0;
+
     while ((c = getchar()) != EOF) {
         if ((c == ' ' || c == '\n' || c == '\t') && state)
         {
@@ -26,14 +31,16 @@ int main()
         }
     }
     printf("The following is the histogram");
-    for (int i=0 ;i<=20 ;++i)
+    for (int i = 0 ;i < 20 ;++i)
     {
         printf("Length %d: ", i + 1);
-        for (int j=0; j<hist[i]; ++j)
+        for (int j = 0; j < hist[i]; ++j)
         {
            printf("*");
-           printf("\n %d %d \n", i, j);
+           //printf("\n %d %d \n", i, j);
+           // printf("Length of hist[i] is %d", hist[i]);
         }
+        printf("\n");
     }
     return 0;
 }

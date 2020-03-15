11afff6d7b398c1b8e60fd672679114f25e6485d
Ongun Arisev <ongunarisev@gmail.com>
2020-03-15 23:32:11 +0100

    Intermediate stage for Ex 1.21 from K&R
    - The code will be tested to see if desired behaviour is achieved

--- ex_1.21_entab.c
+++ ex_1.21_entab.c
@@ -1,27 +1,35 @@
 #include <stdio.h>
 #define TABSTOP 4 /* this is the number of tab stops */
-#define MAXLINE 1000 /* maximum lengths of a line */
 
 /* This program replaces tab stops with the appropriate number of spaces */
 int main()
 {
-    int c, i, j, stop_loc, cts, store=0;
+    int c, i, j, k, l, cts = 1, store=0, offset=0, num_tab=0, in_blank = 0; 
 
-    for (i = 0, j = 0; i < lim - 1 && (c=getchar()) != EOF && c !='\n'; ++i)
-    {
+for (i = 0; i (c=getchar()) != EOF; ++i)
+{
         // Handle the case for tabs here note that the indices are 0 based in C
-        if (c == ' ')
+        if ((c == ' ') && (!in_blank))
         {
-            store++; 
-            /*printf("Blank encountered\n");*/
+            store = i;  // Store the value that the blank starts
+            in_blank = 1; // A blank has been encountered
         }
+        else if ((c == ' ') && (in_blank))
+           ++cts; // Increment a counter to keep track of how many spaces encountered
         else
         {
             if (store != 0)
             {
-               tabs
+                // Being here means we encountered a blank before
+               in_blank = 0;
+                offset =  TABSTOP - (store % TABSTOP);
+                num_tab = cts / TABSTOP;
+                for (j = 0; j < num_tab; ++j)
+                    putchar('\t');
+                for (k = 0; k < offset; ++k)
+                    putchar('*');
+                store = 0;
             }
-            store = 0;
             putchar(c);
         }
     }
@@ -30,7 +38,14 @@ int main()
         // Starting point is extremely important as i is incremented once
         if (store != 0)
         {
-
+                // Being here means we encountered a blank before
+                offset =  (store % TABSTOP);
+                num_tab = store / TABSTOP;
+                for (j = 0; j < num_tab; ++j)
+                    putchar('\t');
+                for (k = 0; k < offset; ++k)
+                    putchar('*');
+                store = 0;
         }
        putchar(c); 
     }

793b0ff54dbf1c70c393768009406e43899ab70e
Ongun Arisev <ongunarisev@gmail.com>
2020-03-16 01:46:27 +0100

    Entab exercise
    + Exercise done in a long style
    + Also the code from the net included

--- ex_1.21_entab.c
+++ ex_1.21_entab.c
@@ -4,50 +4,49 @@
 /* This program replaces tab stops with the appropriate number of spaces */
 int main()
 {
-    int c, i, j, k, l, cts = 1, store=0, offset=0, num_tab=0, in_blank = 0; 
-
-for (i = 0; i (c=getchar()) != EOF; ++i)
-{
+    int c, i, j, k, cts = 0, store=0, offset=0, num_tab=0, in_blank =0;
+    
+    for (i = 1; (c=getchar()) != EOF; ++i)
+    {
         // Handle the case for tabs here note that the indices are 0 based in C
         if ((c == ' ') && (!in_blank))
         {
-            store = i;  // Store the value that the blank starts
-            in_blank = 1; // A blank has been encountered
+            ++cts; // Increment a counter to keep track of how many spaces encountered
+            in_blank = 1;
         }
         else if ((c == ' ') && (in_blank))
-           ++cts; // Increment a counter to keep track of how many spaces encountered
+            ++cts;
         else
         {
-            if (store != 0)
+            if (cts != 0)
             {
                 // Being here means we encountered a blank before
-               in_blank = 0;
-                offset =  TABSTOP - (store % TABSTOP);
-                num_tab = cts / TABSTOP;
+                offset = (cts + store) % TABSTOP;
+                num_tab = (cts + store) / TABSTOP;
+                /*printf("\nThe offset is %d, the numtab is %d and the cts is %d and store is %d\n", offset, num_tab, cts, store);*/
                 for (j = 0; j < num_tab; ++j)
                     putchar('\t');
-                for (k = 0; k < offset; ++k)
+                for (k = 0; k < offset - store; ++k)
                     putchar('*');
-                store = 0;
+                cts = 0;
+                in_blank = 0;
             }
-            putchar(c);
+            if (c != '\n')
+            {
+                store = i;
+                putchar(c);
+            }
+            if (c == '\n')
+            {
+                i = 0;
+                putchar('N');
+                putchar(c);
+            }
+            else if ( c == '\t')
+                i = 0;
+            // Deserves a comment: For loop increases the loop variable afterwards
+            // Just before the next iteration of the for loop
         }
-    }
-    if (c == '\n')
-    {
-        // Starting point is extremely important as i is incremented once
-        if (store != 0)
-        {
-                // Being here means we encountered a blank before
-                offset =  (store % TABSTOP);
-                num_tab = store / TABSTOP;
-                for (j = 0; j < num_tab; ++j)
-                    putchar('\t');
-                for (k = 0; k < offset; ++k)
-                    putchar('*');
-                store = 0;
-        }
-       putchar(c); 
     }
     return 0;
 }
